a2c10e400c360c157d8037c1d6ae77fd719f81c9
cucumber-sponge <johnprocter20@gmail.com>
2026-08-26 23:24:58 -0700

    Add files via upload

---
+++ page.js
@@ -0,0 +1,10 @@
+function lm() {
+    var img = document.getElementById("logo");
+    var des = document.getElementById("dn").innerHTML="Not everyone orders DoorDash three time a day, which makes paying $10.99 every month for DashPass unnecessary for many people. Instead, you can save on your order for just $0.50 per use. There's no monthly subcription, no recurring charges, and no commitment-just a one time 50 cent fee whenever you want to save money on an order.";
+
+    if (!img.style.display || img.style.display === "none") {
+        img.style.display = "none";
+    } else {
+        img.style.display = "none";
+    }
+}
